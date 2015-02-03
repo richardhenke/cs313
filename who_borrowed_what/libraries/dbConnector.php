c250de499b3f284d8c4591807937462c19dc064e
Richard Henke <richthemanofsteel@gmail.com>
2015-02-02 22:38:16 -0700

    Test1 db connect

---
+++ who_borrowed_what/libraries/dbConnector.php
@@ -0,0 +1,39 @@
+<?php
+
+function loadDatabase()
+{
+
+  $dbHost = "127.12.98.2";
+  $dbPort = "3306";
+  $dbUser = "cs313_admin";
+  $dbPassword = "Z35Zxz37mzUeMhRP";
+
+  $dbName = "who_borrowed_what";
+
+     // $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+     // if ($openShiftVar === null || $openShiftVar == "")
+     // {
+     //      // Not in the openshift environment
+     //      //echo "Using local credentials: "; 
+     //      require("setLocalDatabaseCredentials.php");
+     // }
+     // else 
+     // { 
+     //      // In the openshift environment
+     //      //echo "Using openshift credentials: ";
+
+     //      $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+     //      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+     //      $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+     //      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+     // } 
+     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+  echo "host:$dbHost:$dbPort <br>dbName:$dbName <br>user:$dbUser <br>password:$dbPassword<br />\n";
+
+  $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  return $db;
+}
+
+?>
--- who_borrowed_what/test_dbAccess.php
+++ who_borrowed_what/test_dbAccess.php
@@ -31,13 +31,49 @@
 // $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
 // $dbh = new PDO($dsn, DB_USER, DB_PASS);
 
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+// $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+// $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+// $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+// $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 
-echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
+//echo "host:$dbHost:$dbPort <br>dbName:$dbName <br>user:$dbUser <br>password:$dbPassword<br />\n";
+function loadDatabase()
+{
 
+  $dbHost = "127.12.98.2";
+  $dbPort = "3306";
+  $dbUser = "cs313_admin";
+  $dbPassword = "Z35Zxz37mzUeMhRP";
 
+  $dbName = "who_borrowed_what";
+
+     // $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+     // if ($openShiftVar === null || $openShiftVar == "")
+     // {
+     //      // Not in the openshift environment
+     //      //echo "Using local credentials: "; 
+     //      require("setLocalDatabaseCredentials.php");
+     // }
+     // else 
+     // { 
+     //      // In the openshift environment
+     //      //echo "Using openshift credentials: ";
+
+     //      $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+     //      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+     //      $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+     //      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+     // } 
+     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+  echo "host:$dbHost:$dbPort <br>dbName:$dbName <br>user:$dbUser <br>password:$dbPassword<br />\n";
+
+  $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  return $db;
+}
+echo "<br>Loading database<br>";
+loadDatabase();
+echo "<br>did it load?<br>";
 
 ?>
